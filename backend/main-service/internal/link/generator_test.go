28b3ab404b857473b93b865e143a596ad04a8f30
achi3v3 <amir-tutaev@mail.ru>
2025-08-15 22:37:47 +0500

    feat(back): create test for func generating link

---
+++ backend/main-service/internal/link/generator_test.go
@@ -0,0 +1,9 @@
+package link
+
+import "testing"
+
+func TestGeneratePseudoLink(t *testing.T) {
+	if pseudoLink := generatePseudoLink(); !(len(pseudoLink) > 0) {
+		t.Errorf("TestGeneratePseudoLink: error with generationg link %s", pseudoLink)
+	}
+}
