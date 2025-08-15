84c6c3c70071a657dc8194dccd06dfa255cb5946
achi3v3 <amir-tutaev@mail.ru>
2025-08-15 22:35:08 +0500

    add database repository handlers

---
+++ backend/main-service/internal/link/repository.go
@@ -0,0 +1,57 @@
+package link
+
+import (
+	"context"
+	"main-service/backend/main-service/internal/models"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type LinkRepository struct {
+	client *redis.Client
+}
+
+func NewLinkRepository(client *redis.Client) *LinkRepository {
+	return &LinkRepository{client: client}
+}
+
+func (r *LinkRepository) Create(link *models.Link) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return r.client.Set(ctx, link.OriginLink, link.PseudoLink, 0).Err()
+}
+
+func (r *LinkRepository) Find(originLink string) (link *models.Link, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pseudoLink, err := r.client.Get(ctx, originLink).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, nil // key not found
+		}
+		return nil, err
+	}
+
+	return &models.Link{
+		OriginLink: originLink,
+		PseudoLink: pseudoLink,
+	}, nil
+}
+
+func (r *LinkRepository) Delete(originLink string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return r.client.Del(ctx, originLink).Err()
+}
+
+func (r *LinkRepository) Exist(originLink string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	exists, err := r.client.Exists(ctx, originLink).Result()
+	return exists > 0, err
+}
